d2bb34990f77ac4dadd5b8f532542261f37b5d98
Lance R. Vick <lance@lrvick.net>
2016-07-17 00:15:24 -0700

    lowercase all the things (except keys and stuff)

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 -- -*- mode: sql; product: postgres -*-
 
 -- hosts table
-CREATE TABLE "hosts" (
-  "id" serial PRIMARY KEY,
-  "name" text UNIQUE NOT NULL,
+create table "hosts" (
+  "id" serial primary key,
+  "name" text unique not null,
   "location" text,
   -- 4326 is the EPSG standard for lat/lon
   "coordinates" geography (point, 4326),
@@ -13,33 +13,33 @@ CREATE TABLE "hosts" (
 
 -- data for NSS' passwd
 -- there is an implicit primary group for each user
-CREATE SEQUENCE user_id MINVALUE 4000 MAXVALUE 2147483647 NO CYCLE;
+create sequence user_id minvalue 4000 maxvalue 2147483647 no cycle;
 
-CREATE DOMAIN username_t varchar(31) CHECK (
-  VALUE ~ '^[a-z][a-z0-9]+$'
+create domain username_t varchar(31) check (
+  value ~ '^[a-z][a-z0-9]+$'
 );
 
 -- auxiliary groups
-CREATE TABLE "group" (
-  "gid" integer PRIMARY KEY MAXVALUE 999,
-  "name" username_t UNIQUE NOT NULL,
+create table "group" (
+  "gid" integer primary key maxvalue 999,
+  "name" username_t unique not null,
 );
 
-CREATE TABLE "passwd" (
-  "name" username_t UNIQUE NOT NULL,
-  "uid" integer PRIMARY KEY MINVALUE 1000 DEFAULT nextval('user_id'),
+create table "passwd" (
+  "name" username_t unique not null,
+  "uid" integer primary key minvalue 1000 default nextval('user_id'),
   "gid" integer not null references "group" (gid)
     on update cascade on delete cascade,
   "gecos" text, 
   -- Every user must have a default host for ssh/mail routing
-  "host" integer not null REFERENCES hosts (id),
+  "host" integer not null references hosts (id),
   -- This could be any folder as we could also have bot users like 'git'
-  "homeDir" text NOT NULL,
+  "homeDir" text not null,
   "sshKeys" text[] not null,
 );
 
-CREATE TABLE "aux_groups" (
-  "uid" int4 NOT NULL REFERENCES passwd (uid) ON DELETE CASCADE,
-  "gid" int4 NOT NULL REFERENCES group  (gid) ON DELETE CASCADE,
-  PRIMARY KEY ("uid", "gid"),
+create table "aux_groups" (
+  "uid" int4 not null references passwd (uid) on delete cascade,
+  "gid" int4 not null references group  (gid) on delete cascade,
+  primary key ("uid", "gid"),
 );

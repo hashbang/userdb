109a11e83dbfc390891887d15a45dadeffa06e0d
The Fox in the Shell <KellerFuchs@hashbang.sh>
2016-07-19 23:08:35 +0200

    Test Postfix's host query

--- test.sql
+++ test.sql
@@ -50,4 +50,18 @@ BEGIN
 END $$ LANGUAGE plpgsql;
 
 
+-- Query used by Postfix
+CREATE FUNCTION unit_tests.postfix()
+RETURNS test_result AS $$
+DECLARE message test_result;
+DECLARE result boolean;
+DECLARE passwd_host text;
+BEGIN
+    SELECT host FROM passwd WHERE name = 'testuser' INTO passwd_host;
+    SELECT * FROM assert.is_equal(passwd_host, 'testbox.hashbang.sh') INTO message, result;
+
+    IF result = false THEN RETURN message; END IF;
+    SELECT assert.ok('End of test.') INTO message; RETURN message;
+END $$ LANGUAGE plpgsql;
+
 SELECT * FROM unit_tests.begin();

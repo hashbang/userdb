57733e81164fe641c9c893e18c62f43aaf19b299
The Fox in the Shell <KellerFuchs@hashbang.sh>
2017-03-13 20:41:41 -0400

    test.sh: Clearer run output

--- test.sh
+++ test.sh
@@ -1,17 +1,25 @@
-#!/bin/sh -ex
+#!/bin/sh -e
+
+run() {
+    normal='\e[0m'
+    yellow='\e[33m'
+    printf "${yellow}%s${normal}\n" "$*" >&2
+
+    "$@"
+}
 
 if ! command -v initdb 2>/dev/null; then
     echo "No PostgreSQL utilities in PATH" >&2
     exit 1
 fi
 
-git submodule update
+run git submodule update
 
 trap 'pg_ctl -D "${WORKDIR}" stop; rm -rf -- "${WORKDIR}"' EXIT
 WORKDIR=$(mktemp -d)
 
-initdb -D "${WORKDIR}"
-pg_ctl -D "${WORKDIR}" start -w -o "          \
+run initdb -D "${WORKDIR}"
+run pg_ctl -D "${WORKDIR}" start -w -o "      \
 	-c unix_socket_directories=${WORKDIR} \
 	-c listen_addresses=''                \
 "
@@ -19,16 +27,16 @@ pg_ctl -D "${WORKDIR}" start -w -o "          \
 
 PSQL="psql --set ON_ERROR_STOP=1 -h ${WORKDIR} -d postgres"
 
-${PSQL} -f schema.sql
+run ${PSQL} -f schema.sql
 
 if [ "$1" = 'develop' ]; then
-    psql -h "${WORKDIR}" -d postgres
+    run psql -h "${WORKDIR}" -d postgres
 else
     for file in tests/plpgunit/install/1.install-unit-test.sql tests/*.sql
     do
-	${PSQL} -f "$file"
+	run ${PSQL} -f "$file"
     done
 
-    ${PSQL} -c 'SELECT * FROM unit_tests.begin();' | tee "${WORKDIR}/log"
+    run ${PSQL} -c 'SELECT * FROM unit_tests.begin();' | tee "${WORKDIR}/log"
     grep -q 'Failed tests *: 0.' "${WORKDIR}/log"
 fi

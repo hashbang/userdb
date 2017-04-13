a4fe38da1d307e502f8b2155d611949e1f604410
The Fox in the Shell <KellerFuchs@hashbang.sh>
2017-04-13 00:21:15 -0400

    json-schemas.py: Assert $$ is absent from JSON
    $$ is used in the PgSQL file to quote the JSON data.

--- json-schemas.py
+++ json-schemas.py
@@ -7,6 +7,7 @@ with open('json-schemas.sql', 'r') as in_file:
 for name in ('user', 'host'):
     with open("schemas/data_%s.yml" % name, 'r') as schema:
         json_data = json.dumps(yaml.load(schema), indent=4)
+        assert("$$" not in json_data)
         data = string.replace(data, '{data_%s}' % name, json_data)
 
 with open('json-schemas.sql.tmp', 'w') as outfile:

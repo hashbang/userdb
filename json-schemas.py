56e99a014f8bfe37011b35ec69dad5875d5c96af
daurnimator <quae@daurnimator.com>
2018-03-18 11:40:04 +1100

    json-schemas.py: string.replace is deprecated

--- json-schemas.py
+++ json-schemas.py
@@ -8,7 +8,7 @@ for name in ('user', 'host'):
     with open("schemas/data_%s.yml" % name, 'r') as schema:
         json_data = json.dumps(yaml.load(schema), indent=4)
         assert("$$" not in json_data)
-        data = string.replace(data, '{data_%s}' % name, json_data)
+        data = data.replace('{data_%s}' % name, json_data)
 
 with open('json-schemas.sql.tmp', 'w') as outfile:
     outfile.write(data)

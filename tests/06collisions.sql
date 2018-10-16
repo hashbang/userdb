ee28aacd7eb298ecb0ab9ab6473a07f8470ea549
RyanSquared <ryan@hashbang.sh>
2018-10-15 22:51:38 -0500

    tests/06collisions: Add fail-check cases for #17 #28

---
+++ tests/06collisions.sql
@@ -0,0 +1,45 @@
+-- Negative tests for `check_hosts_for_hosts`, `check_invalid_name_for_passwd`,
+-- `check_invalid_name_for_group`, `check_reserved_username`, and
+-- `check_taken_username`
+
+create function unit_tests.check_hosts_for_hosts()
+returns test_result as $$
+begin
+    insert into hosts (name, maxusers) values ('06invalid.hashbang.sh', 1);
+    insert into passwd (name, host) values ('ryan', '06invalid.hashbang.sh');
+    begin
+        update hosts set maxusers = 0 where name = '06invalid.hashbang.sh';
+        return assert.fail('Successfully changed maxusers to be less than users');
+    exception
+    when foreign_key_violation then
+        return assert.ok('End of test.');
+    end;
+end $$ language plpgsql;
+
+create function unit_tests.check_invalid_name_for_passwd()
+returns test_result as $$
+begin
+    insert into hosts (name, maxusers) values ('061invalid.hashbang.sh', 1);
+    insert into "group" (gid, name) values (600, 'grouptest06');
+    begin
+        insert into passwd (name, host) values ('grouptest06', '061invalid.hashbang.sh');
+        return assert.fail('Successfully added group name collision from user');
+    exception
+    when check_violation then
+        return assert.ok('End of test.');
+    end;
+end $$ language plpgsql;
+
+create function unit_tests.check_invalid_name_for_group()
+returns test_result as $$
+begin
+    insert into hosts (name, maxusers) values ('062invalid.hashbang.sh', 1);
+    insert into passwd (name, host) values ('passwdtest06', '062invalid.hashbang.sh');
+    begin
+        insert into "group" (gid, name) values (601, 'passwdtest06');
+        return assert.fail('Successfully added passwd name collision from group');
+    exception
+    when check_violation then
+        return assert.ok('End of test.');
+    end;
+end $$ language plpgsql;

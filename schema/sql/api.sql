6451f3adc303e1ad2652b8b756230e9ffa6dc978
Lance R. Vick <lance@lrvick.net>
2020-01-26 14:30:48 -0800

    move alter view and create user statements per request from @daurnimator

--- schema/sql/api.sql
+++ schema/sql/api.sql
@@ -1,70 +1,9 @@
 -- -*- mode: sql; sql-product: postgres -*-
 
-create schema v1;
-
-create view v1.hosts as
-    select
-        hosts.name,
-        hosts.maxusers,
-        hosts.data
-    from public.hosts;
-comment on view v1.hosts is
-    $$Contains the hashbang servers$$;
-comment on column v1.hosts.name is
-    $$Host's domain name$$;
-comment on column v1.hosts.maxusers is
-    $$The maximum users supported by this server$$;
-comment on column v1.hosts.data is
-    $$Extra data added in the stats answer.$$;
-
-create view v1.passwd as
-    select
-        uid,
-        name,
-        host,
-        data
-    from public.passwd;
-comment on view v1.passwd is
-    $$Users$$;
-comment on column v1.passwd.uid is
-    $$User's unique ID$$;
-comment on column v1.passwd.host is
-    $$User's 'home' host$$;
-comment on column v1.passwd.data is
-    $$Extra user data$$;
-
-create view v1."group" as
-    select
-        gid,
-        name
-    from public."group";
-comment on view v1."group" is
-    $$Groups$$;
-comment on column v1."group".gid is
-    $$Group ID$$;
-comment on column v1."group".name is
-    $$Group Name$$;
-
-create view v1.aux_groups as
-    select
-        uid,
-        gid
-    from public.aux_groups;
-comment on view v1.aux_groups is
-    $$User<>Group relationships$$;
-comment on column v1.aux_groups.uid is
-    $$User ID$$;
-comment on column v1.aux_groups.gid is
-    $$Group ID$$;
-
 create user "api" noinherit;
 comment on role api is
     $$API role used for API abstractions like PostgREST$$;
 alter role "api" with login;
-alter view v1."hosts" owner to api;
-alter view v1."passwd" owner to api;
-alter view v1."group" owner to api;
-alter view v1."aux_groups" owner to api;
 grant usage on schema public to api;
 grant create,usage on schema v1 to api;
 grant select,insert,update,delete on table
@@ -96,3 +35,64 @@ grant usage on sequence "user_id" to "api-user-create";
 grant insert("name","host","data") on table v1."passwd" to "api-user-create";
 grant "api-user-create" to "api";
 grant "api-anon" to "api-user-create";
+
+create schema v1;
+
+create view v1.hosts as
+    select
+        hosts.name,
+        hosts.maxusers,
+        hosts.data
+    from public.hosts;
+comment on view v1.hosts is
+    $$Contains the hashbang servers$$;
+comment on column v1.hosts.name is
+    $$Host's domain name$$;
+comment on column v1.hosts.maxusers is
+    $$The maximum users supported by this server$$;
+comment on column v1.hosts.data is
+    $$Extra data added in the stats answer.$$;
+alter view v1."hosts" owner to api;
+
+create view v1.passwd as
+    select
+        uid,
+        name,
+        host,
+        data
+    from public.passwd;
+comment on view v1.passwd is
+    $$Users$$;
+comment on column v1.passwd.uid is
+    $$User's unique ID$$;
+comment on column v1.passwd.host is
+    $$User's 'home' host$$;
+comment on column v1.passwd.data is
+    $$Extra user data$$;
+alter view v1."passwd" owner to api;
+
+create view v1."group" as
+    select
+        gid,
+        name
+    from public."group";
+comment on view v1."group" is
+    $$Groups$$;
+comment on column v1."group".gid is
+    $$Group ID$$;
+comment on column v1."group".name is
+    $$Group Name$$;
+alter view v1."group" owner to api;
+
+create view v1.aux_groups as
+    select
+        uid,
+        gid
+    from public.aux_groups;
+comment on view v1.aux_groups is
+    $$User<>Group relationships$$;
+comment on column v1.aux_groups.uid is
+    $$User ID$$;
+comment on column v1.aux_groups.gid is
+    $$Group ID$$;
+alter view v1."aux_groups" owner to api;
